c7f1a8752615453d6fd7cac18e611df0ab1112b0
mjimeg85 <mjimeg85@gmail.com>
2021-08-13 18:46:38 +0200

    Se agrega la entidad de Uusario y su relacion con Staff y Pasajero.

--- src/main/java/ar/com/ada/api/aladas/entities/Pasajero.java
+++ src/main/java/ar/com/ada/api/aladas/entities/Pasajero.java
@@ -16,6 +16,9 @@ public class Pasajero extends Persona {
     @OneToMany(mappedBy = "pasajero", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Reserva> reservas = new ArrayList<>();
 
+    @OneToOne(mappedBy = "pasajero", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Usuario usuario;
+
     public Integer getPasajeroId() {
         return pasajeroId;
     }
@@ -24,11 +27,27 @@ public class Pasajero extends Persona {
         this.pasajeroId = pasajeroId;
     }
 
+    public List<Reserva> getReservas() {
+        return reservas;
+    }
+
+    public void setReservas(List<Reserva> reservas) {
+        this.reservas = reservas;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+        usuario.setPasajero(this);
+    }
+
     public void agregarReserva(Reserva reserva) {
         this.reservas.add(reserva);
         reserva.setPasajero(this);
     }
-
 
 
 }
